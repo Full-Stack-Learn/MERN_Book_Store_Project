23c4a43973935da1eae3f41ba282f876d2c15fbb
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-08-07 23:57:36 +0530

    connect mongodb and some changes in backend

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+   'mongodb+srv://2021cs125:Chathu4343@book-store-mern.evo5p4j.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-Mern'
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
