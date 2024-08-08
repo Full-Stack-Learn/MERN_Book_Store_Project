fba4504e6e5ba10aae15dad4fae11976f9ee6e3f
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-08-08 12:35:04 +0530

    Create routes and cors

--- backend/index.js
+++ backend/index.js
@@ -1,130 +1,37 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js"
 import mongoose from "mongoose";
-import { Book } from './models/bookModel.js'
+import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
 
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handling CORS POLICY
+// Option 1: Allow All origins with Default of cors(*)
+// app.use(cors());
+
+//Option 2: Allow Custom origins
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST','PUT','DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/',(request,response) => {
     console.log(request);
     return response.status(234).send('Welcome to MERN Stack Tutorial');
 })
 
-// Route for Save a new Book
+app.use('/books', booksRoute);
 
-app.post('/books', async(request, response) => {
-    try {
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title,author,publishyear',
-            }); 
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-
-        const book = await Book.create(newBook);
-        return response.status(201).send(book);
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message}); 
-    }
-});
-
-//Route for get all  books from database
-app.get('/books' , async (request, response)=> {
-    try {
-        const books = await Book.find({});
-
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        });
-        
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message :error.message})
-        
-    }
-});
-
-//Route for get one  book from database by id
-app.get('/books/:id' , async (request, response)=> {
-    try {
-        const { id } =request.params;
-        const book = await Book.findById(id);
-
-        return response.status(200).json(book );
-        
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message :error.message})
-        
-    }
-});
-
-// Route for update a book
-app.put('/books/:id',async (request,response) => {
-    try {
-
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title,author,publishyear',
-            }); 
-        }
-        
-        const { id } = request.params;
-
-        const result = await Book.findByIdAndUpdate(id , request.body);
-
-        if(!result){
-            return response.status(404).json({message:'Book not found'});
-        }
-
-        return response.status(200).send({message: 'Book update successfully'});
-
-
-    } catch (error) 
-    {
-     console.log(error.message);
-     response.status(500).send({message:error.message});   
-    }
-});
-
-// Route for delete a book
-
-app.delete('books/:id' , async (request, response) => {
-    try {
-        const { id } =request.params;
-
-        const result = await Book.findByIdAndDelete(id);
-
-        if(!result){
-            return response.status(404).json({message:'Book not found'});
-        }
-        
-        return response.status(200).send({message: 'Book delete successfully'});
-
-        
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message:error.message});  
-    }
-})
 
 
 mongoose
